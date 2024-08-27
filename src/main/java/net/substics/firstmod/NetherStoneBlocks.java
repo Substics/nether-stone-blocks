53cdbf15ef67fe07554da34c4ae4c1fbe9c69c0a
Substics <adamkamak2020@gmail.com>
2024-08-27 22:59:56 +0100

    Initial commit

---
+++ src/main/java/net/substics/firstmod/NetherStoneBlocks.java
@@ -0,0 +1,16 @@
+package net.substics.firstmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NetherStoneBlocks implements ModInitializer {
+	public static final String MOD_ID = "nether-stone-blocks";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/substics/firstmod/NetherStoneClient.java
@@ -0,0 +1,10 @@
+package net.substics.firstmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NetherStoneClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
