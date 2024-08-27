376a2941ca0158e5943e1481a161740b9ce184ce
Substics <adamkamak2020@gmail.com>
2024-08-27 23:58:13 +0100

    fixed stuff again 2?

--- src/main/java/net/substics/firstmod/item/ModItems.java
+++ src/main/java/net/substics/firstmod/item/ModItems.java
@@ -2,7 +2,6 @@ package net.substics.firstmod.item;
 
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
